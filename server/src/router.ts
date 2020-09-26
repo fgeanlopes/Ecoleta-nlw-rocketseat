ce57905c39ffa8af9b1c0baa98349bd6365e1a33
Gean Lopes <f.geanlopes@gmail.com>
2020-09-26 16:01:13 -0300

    insert do primeiro local cadastrado para coleta no banco

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,12 +7,46 @@ routes.get('/items', async (req, res) => {
 
     const serializedItems = items.map(item =>{
         return {
+          id:item.id,
           title: item.title,
           img_url: `http://localhost:3333/uploads/${item.image}`,
         };
     })
 
     return res.json({ serializedItems });
+});
+
+routes.post('/points', async (req, res)=>{
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+  await knex("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+
+    // image: "image-fake",
+    // name:"gean",
+    // email: "f.geanlopes@gmail.com",
+    // whatsapp: "1954156156",
+    // latitude: "51616156",
+    // longitude: "89451",
+    // city: "americana",
+    // uf: "ssp",
+  });
+  return res.json({sucess:true})
 });
 
 export default routes;
