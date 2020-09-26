a488195c83d2d2aa2d4a238dbc90c91fccdf6624
Gean Lopes <f.geanlopes@gmail.com>
2020-09-26 14:08:10 -0300

    Listar items do banco, ok

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,10 +3,14 @@ import path from 'path';
 module.exports = {
   client: "sqlite3",
   connection: {
-    filename: path.resolve(__dirname, "src", "database" , "databse.sqlite"),
+    filename: path.resolve(__dirname, "src", "database", "databse.sqlite"),
   },
-  migrations:{
-      directory: path.resolve(__dirname, "src", "database", 'migrations')
+  migrations: {
+    directory: path.resolve(__dirname, "src", "database", "migrations"),
   },
+  seeds: {
+    directory: path.resolve(__dirname, "src", "database", "seeds"),
+  },
+
   useNullAsDefault: true,
 };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import knex from './database/connection';
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "hello word" });
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item =>{
+        return {
+          title: item.title,
+          img_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    })
+
+    return res.json({ serializedItems });
 });
 
 export default routes;
