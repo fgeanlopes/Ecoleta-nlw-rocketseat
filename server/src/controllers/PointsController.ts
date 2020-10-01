699266dc6a64e3574be8b419122ba3e53e6d39de
Gean Lopes <f.geanlopes@gmail.com>
2020-09-30 23:23:04 -0300

    Listagem de items, busca dos points por id

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,46 +1,53 @@
 import {Request, Response} from "express";
 import knex from "../database/connection";
 
-class PointsController{
-    async create (req: Request, res: Response){
-        const {
-            name,
-            email,
-            whatsapp,
-            latitude,
-            longitude,
-            city,
-            uf,
-            items
-        } = req.body;
-
-
-        const point = {
-          image: "image-fake",
-          name,
-          email,
-          whatsapp,
-          latitude,
-          longitude,
-          city,
-          uf,
-        };
-        const insert_id = await knex("points").insert(point);
-
-        
-        const point_id = insert_id[0];
-        const pointItems = items.map((item_id: number) =>{
-            return {
-            item_id,
-            point_id,
-            };
-        });
-        await knex("point_items").insert(pointItems);
-            return res.json({
-                id:point_id,
-                ...point,
-            });
+class PointsController {
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const point = await knex("points").where("id", id).first();
+    if (!point) {
+      return response.status(400).json({ message: "points not found" });
     }
+    return response.json(point);
+  }
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items,
+    } = request.body;
+
+    const point = {
+      image: "image-fake",
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+    };
+    const insert_id = await knex("points").insert(point);
+
+    const point_id = insert_id[0];
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id,
+      };
+    });
+    await knex("point_items").insert(pointItems);
+    return response.json({
+      id: point_id,
+      ...point,
+    });
+  }
 }
 
 export default PointsController;
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,25 +1,17 @@
 import express from "express";
-import knex from './database/connection';
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (req, res) => {
-    const items = await knex('items').select('*');
+routes.get("/items", itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
-    const serializedItems = items.map(item =>{
-        return {
-          id:item.id,
-          title: item.title,
-          img_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    })
-
-    return res.json({ serializedItems });
-});
-
-routes.post('/points',pointsController.create);
+// METODOS PARA ROTAS
+// index, show, create, update, delete
 
 export default routes;
