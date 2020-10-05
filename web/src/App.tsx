d705005a522f3f0bf53dabb7460fd7234d75f132
Gean Lopes <f.geanlopes@gmail.com>
2020-10-05 09:05:06 -0300

    Defindo os tipos dos componentes criados

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header'
+
 function App() {
   return (
-   <h1>Hello Word</h1>
+    <Header title="Hello Word" numberSold={999}/>
   );
 }
 
