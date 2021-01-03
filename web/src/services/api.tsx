b26097173a33b04b10aeac798682657ddc47ee97
Gean Otávio Lopes Ferraz <f.geanlopes@gmail.com>
2021-01-03 11:51:14 -0300

    base url

--- web/src/services/api.tsx
+++ web/src/services/api.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import axios from 'axios';
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
 
 const api = axios.create({
     baseURL:'https://ecoleta-nlw-rocketseat.vercel.app/'
